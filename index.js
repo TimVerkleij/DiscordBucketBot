8a2d38caa16fdca9de9ef0ee8bf5accd8e953c86
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2020-09-02 21:15:27 +0200

    Set bot activity

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.once('ready', () => {
-	console.log('Ready!');
+client.on("ready", () => {
+    client.user.setActivity(`Use !penis`);
+    console.log("Ready!")
 });
 
 
 client.on('message', message => {
-    var listenTo = message.content.slice(0,6)
-    console.log(listenTo)
+    var listenTo = message.content.slice(0, 6)
+    var startCommand = message.content.slice(0, 1)
     if (listenTo === '!penis') {
         // send back "Pong." to the channel the message was sent in
-        message.channel.send(message.member.displayName + " has a penis length of " + Math.floor(Math.random()* 10 + 1) + " inches.");
+        message.channel.send(message.member.displayName + " has a penis length of " + Math.floor(Math.random() * 10 + 1) + " inches.");
+    } else if (startCommand == "!") {
+        message.channel.send("the only available command right now is !penis Bucket will be adding more commands later!")
     }
 });
+
+
 
 client.login('NzUwNjY3MjM1Njg0NTE1ODcy.X093Vw.e3UuaU6Uj8dHcc7MUXvVlDc_9cU');
