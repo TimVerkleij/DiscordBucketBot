ad6ff20f44d2c98b543564b53952cb0ab41b588a
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2020-09-03 20:21:23 +0200

    Cleanup code

--- index.js
+++ index.js
@@ -6,26 +6,6 @@ client.on("ready", () => {
     console.log("Ready!")
 });
 
-
-// client.on('message', message => {
-//     if (message.member.id != "@750667235684515872") {
-//         var listenTo = message.content.slice(0, 6)
-//         var startCommand = message.content.slice(0, 1)
-//         if (listenTo === '!penis') {
-//             // send back "Pong." to the channel the message was sent in
-//             message.channel.send(message.member.displayName + " has a penis length of " + Math.floor(Math.random() * 10 + 1) + " inches.");
-//         } else if (listenTo === '!stink') {
-//             // send back "Pong." to the channel the message was sent in
-//             message.channel.send("You suck " + "@");
-//         } else if (listenTo === "callme") {
-//             message.channel.send("<@" + message.member.id + ">")
-//         } else if (startCommand == "!") {
-//             message.channel.send("the only available command right now is !penis Bucket will be adding more commands later!")
-//         }
-//     }
-// });
-
-
 client.on('message', message => {
     if (message.member.id != "750667235684515872") {
 
