898bb98590dcea7fb52e7766313e687b5f221961
Mitchell <contact@mitchells.work>
2021-02-06 18:52:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -566,23 +566,22 @@ function checkGameEnd(array, message) {
 
 //connects to a voice channel and plays a requested song
 async function play(voiceChannel, song, message) {
-        if(song.startsWith("https://")){
-            const stream = ytdl(song, {
-                filter: 'audioonly',
-                highWaterMark: 1 << 25
-            });
-        } else {
-            return message.channel.send("Video not found. Better luck next time");
-        }
-        
-        try {
-            const connection = await voiceChannel.join(); // ?connect
-            const dispatcher = connection.play(stream)
-            dispatcher.on('finish', () => (voiceChannel.leave()));
-        } catch (err) {
-            message.channel.send("Could not connect to your voice channel. Do I have the right permissions? Or are you just being a dick? :frowning: \n Consider asking a moderator to give me permissions :wink:");
-            throw err;
-        }
+    // has to be url even though this is a bad check...
+    if (!/https?:\/\/youtu(\.be|be\.)/i.test(song)) return message.channel.send("Where is the url?");
+
+    const stream = ytdl(song, {
+        filter: 'audioonly',
+        highWaterMark: 1 << 25
+    });
+    if (!stream) return message.channel.send("Video not found. Better luck next time");
+
+    try {
+        const connection = await voiceChannel.join(); // ?connect
+        const dispatcher = connection.play(stream)
+        dispatcher.on('finish', () => voiceChannel.leave());
+    } catch {
+        message.channel.send("Could not connect to your voice channel. Do I have the right permissions? Or are you just being a dick? :frowning: \n Consider asking a moderator to give me permissions :wink:");
+    }
 }
 
 
